093da4aba720656e0f21a19c09c95ad69a57b89c
Corona Laufer <laufer.corona@hotmail.com>
2021-03-04 13:31:54 +0100

    feat: rendering images

--- client/screens/ProjectFolder.js
+++ client/screens/ProjectFolder.js
@@ -1,19 +1,91 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, ImageBackground, TouchableOpacity, View, Text, FlatList  } from 'react-native';
+
+
+export default function ProjectFolder () {
+  let counter = 1;
+  return (
+    <ImageBackground source={require('../assets/Background.png')} style={styles.image}>
+      <TouchableOpacity style={styles.button} >
+        <Text style={styles.buttonText}> Add Step </Text>
+      </TouchableOpacity>
+      <FlatList
+        horizontal={false}
+        style={styles.container}
+        numColumns = {2}
+        data={mockFolders}
+        // eslint-disable-next-line react/no-unescaped-entities
+        ListEmptyComponent={<Text style={styles.text}>Click "Add Step" to get started</Text>}
+        keyExtractor={item => item.id}
+        renderItem={ ({ item }) => (
+          <View>
+            <TouchableOpacity>
+              <Image source={{uri: item.imageUrl}} style = {styles.ImagesStyle} />
+            </TouchableOpacity>
+            <Text style={styles.text}> Step {counter++}</Text>
+          </View>
+        )}>
+      </FlatList>
+    </ImageBackground>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 30,
+  },
+  image: {
     flex: 1,
-    backgroundColor: '#fff',
+    resizeMode: 'cover',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  ImagesStyle: {
+    height: 160,
+    width: 160,
+    margin: 10,
+    borderRadius: 5,
+  },
+  text: {
+    marginBottom: 10,
+    padding: 5,
+    color: 'white',
+    fontSize: 15,
+
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 85,
+    width: 320,
+    padding: 17,
+    borderRadius: 15,
+    marginTop: 15,
+    backgroundColor: '#FFDE59',
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    color: '#342F1E',
+  },
 });
 
-export default function ProjectFolder () {
-  return (
-    <View style={styles.container}>
-      <Text>Honda</Text>
-    </View>
-  );
-}
+const mockFolders = [
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f65',
+    title: 'Honda',
+    timeStamp: 1,
+    imageUrl: 'https://i.ytimg.com/vi/7lhzTBn_TqM/maxresdefault.jpg'
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Project',
+    timeStamp: 2,
+    imageUrl: 'https://i.ytimg.com/vi/BUmbBcVGvWw/maxresdefault.jpg',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-141e29d72',
+    title: 'Third Project',
+    timeStamp: 3,
+    imageUrl: 'https://i.ytimg.com/vi/vLTOx71mL_8/maxresdefault.jpg',
+  }
+];
